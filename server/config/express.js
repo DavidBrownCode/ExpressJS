df8819b6c7b77b0b9f1f2d95b5722b6c9db82c78
David Brown <dab0304@gmail.com>
2018-12-17 21:03:14 -0600

    installed package.json globallye

---
+++ server/config/express.js
@@ -0,0 +1,113 @@
+//setting requires to variables
+var express = require('express');
+var morgan = require('morgan');
+var logger = require('./logger');
+var bodyParser = require('body-parser');
+var glob = require('glob');
+var bluebird = require('bluebird');
+var mongoose = require('mongoose');
+
+//add in after testing
+var cors = require('cors');
+
+module.exports = function (app, config) {
+
+    logger.log('info', "Loading Mongoose functionality");
+    mongoose.Promise = bluebird;
+    mongoose.connect(config.db);
+    var db = mongoose.connection;
+    db.on('error', function () {
+        throw new Error('unable to connect to database at ' + config.db);
+    });
+
+    app.use(cors({ origin: 'http://localhost:9000' })); 
+
+    if (process.env.NODE_ENV !== 'test') {
+        app.use(morgan('dev'));
+    
+        mongoose.set('debug', true);
+        mongoose.connection.once('open', function callback() {
+          logger.log('info', 'Mongoose connected to the database');
+        });
+    
+        app.use(function (req, res, next) {
+          logger.log('Request from ' + req.connection.remoteAddress, 'info');
+          next();
+        });
+      }
+    
+    
+    
+    
+      app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(bodyParser.json());
+    app.use(express.static(config.root + '/public'));
+    /*app.use(function (req, res) {
+        res.type('text/plan');
+        res.status(404);
+        res.send('404 Not Found');
+    });
+    app.use(function (err, req, res, next) {
+        console.error(err.stack);
+        res.type('text/plan');
+        res.status(500);
+        res.send('500 Sever Error');
+    });
+    console.log("Starting application");*/
+    var models = glob.sync(config.root + '/app/models/*.js');
+    models.forEach(function (model) {
+      require(model);
+    });
+  
+
+    var controllers = glob.sync(config.root + '/app/controllers/*.js');
+    controllers.forEach(function (controller) {
+      require(controller)(app, config);
+    });
+
+    app.get('/willwork',
+    function (req, res, next) {
+      res.set('X-One', 'One');
+      next();
+    },
+    function (req, res, next) {
+      res.set('X-Two', 'Two');
+      next();
+    },
+    function (req, res) {
+      res.send("Three");
+    }
+  );
+
+
+
+
+
+
+
+    app.use(function (req, res) {
+        logger.log('error', 'File not found');
+        res.type('text/plan');
+        res.status(404);
+        res.send('404 Not Found');
+    });
+    app.use(function (err, req, res, next) {
+        console.log(err);
+        if (process.env.NODE_ENV !== 'test') logger.log(err.stack, 'error');
+        res.type('text/plan');
+        if (err.status) {
+          res.status(err.status).send(err.message);
+        } else {
+          res.status(500).send('500 Sever Error');
+        }
+    });
+    
+    
+    
+    logger.log('info', "Starting application");
+    
+};
+
